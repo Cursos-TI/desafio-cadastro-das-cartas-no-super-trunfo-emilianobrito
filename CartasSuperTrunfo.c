0980e3e22e2685590f2b760faaaf5b955dc90f06
emilianobrito <119542405+emilianobrito@users.noreply.github.com>
2025-06-02 19:49:58 +0000

    Desafio: nível mestre (Super Trunfo em C)

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,6 +1,6 @@
 // Super Trunfo em C - Países
 // Tema 1 - Cadastro das Cartas
-// Desafio: nível novato
+// Desafio: nível mestre
 
 #include <stdio.h>
 
@@ -10,23 +10,34 @@ int main()
     char estado_carta1; 
     char codigo_carta1[4];
     char cidade_carta1[50];
-    int populacao_carta1;
+    unsigned long int populacao_carta1;
     float area_carta1;
     float pib_carta1;
     int pontos_turisticos_carta1;
     float densidade_populacional_carta1;
     float pib_per_capita_carta1;
+    float super_poder_carta1;
 
     // Declaração das variáveis da carta 2
     char estado_carta2;
     char codigo_carta2[4];
     char cidade_carta2[50];
-    int populacao_carta2;
+    unsigned long int populacao_carta2;
     float area_carta2;
     float pib_carta2;
     int pontos_turisticos_carta2;
     float densidade_populacional_carta2;
     float pib_per_capita_carta2;
+    float super_poder_carta2;
+
+    // Declaração das variáveis dos resultados
+    int resultado_populacao;
+    int resultado_area;
+    int resultado_pib;
+    int resultado_pontos_turisticos;
+    int resultado_densidade_populacional;
+    int resultado_pib_per_capita;
+    int resultado_super_poder;
 
     // leitura de dados da Carta 1
     printf("Digite o estado da Carta 1 (escolha uma letra de 'A' a 'H' para representa-lo): ");
@@ -86,6 +97,23 @@ int main()
     pib_per_capita_carta1 = (float) pib_carta1*1000000000/populacao_carta1;
     pib_per_capita_carta2 = (float) pib_carta2*1000000000/populacao_carta2;
 
+    //Calcula o Super Poder da Carta 1 
+    super_poder_carta1 = (float)populacao_carta1+area_carta1+pib_carta1+
+                         (float)pontos_turisticos_carta1+(1/densidade_populacional_carta1)+pib_per_capita_carta1;  
+                         
+    //Calcula o Super Poder da Carta 2 
+    super_poder_carta2 = (float)populacao_carta2+area_carta2+pib_carta2+
+                         (float)pontos_turisticos_carta2+(1/densidade_populacional_carta2)+pib_per_capita_carta2; 
+
+    //Resultados das comparações
+    resultado_populacao = populacao_carta1 > populacao_carta2;
+    resultado_area = area_carta1 > area_carta2;
+    resultado_pib = pib_carta1 > pib_carta2;
+    resultado_pontos_turisticos = pontos_turisticos_carta1 > pontos_turisticos_carta2;
+    resultado_densidade_populacional = densidade_populacional_carta1 < densidade_populacional_carta2;
+    resultado_pib_per_capita = pib_per_capita_carta1 > pib_per_capita_carta2;
+    resultado_super_poder = super_poder_carta1 > super_poder_carta2;
+
     // Impressão da Carta 1
     printf("\nCarta 1:\n");
     printf("Estado: %c\n", estado_carta1);
@@ -97,6 +125,7 @@ int main()
     printf("Número de pontos turísticos: %d\n", pontos_turisticos_carta1);
     printf("Densidade Populacional: %.2f hab/Km²\n", densidade_populacional_carta1); //8.102,63
     printf("PIB per Capita: %.2f reais\n", pib_per_capita_carta1); //56.736,71
+    printf("Super poder da carta 1: %.2f\n", super_poder_carta1);
 
     // Impressão da Carta 2
     printf("\nCarta 2:\n");
@@ -109,6 +138,17 @@ int main()
     printf("Número de pontos turísticos: %d\n", pontos_turisticos_carta2);
     printf("Densidade Populacional: %.2f hab/Km²\n", densidade_populacional_carta2); //5.622,16
     printf("PIB per Capita: %.2f reais\n", pib_per_capita_carta2); //44.531,71
+    printf("Super poder da carta 2: %.2f\n", super_poder_carta2);
+
+    //Impressão dos resultados
+    printf("\nComparação de Cartas:\n");
+    printf("População: %d\n", resultado_populacao);
+    printf("Área: %d\n", resultado_area);
+    printf("PIB: %d\n", resultado_pib);
+    printf("Pontos Turísticos: %d\n", resultado_pontos_turisticos);
+    printf("Densidade Populacional: %d\n", resultado_densidade_populacional);
+    printf("PIB per Capita: %d\n", resultado_pib_per_capita);
+    printf("Super Poder: %d\n", resultado_super_poder);
 
     return 0;
 }
